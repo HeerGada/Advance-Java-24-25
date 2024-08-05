6c67bcd3156847b993a722fea0b888af061fde43
HeerGada <gadaheer718@gmail.com>
2024-08-05 08:10:45 +0530

    Add files via upload

---
+++ Prac2b/src/java/Page3Servlet.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+public class Page3Servlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Page3Servlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+             out.println("<body bgcolor=blue>");
+             Cookie[]ck=request.getCookies();
+             for (int i=0;i<ck.length;i++)
+            {
+                if(ck[i].getName().equals("visit"))
+                {
+                    int count=Integer.parseInt(ck[i].getValue())+1;
+                    out.println("<h1>Visit no:"+count+"</h1>");
+                    ck[i]=new Cookie("visit",count+"");
+                    response.addCookie(ck[i]);
+                }
+                else
+                {
+                    out.println("Welcome "+ck[i].getValue());
+                }
+            }
+             out.println( "<br><a href='Page2Servlet'>Click here for page2</a><br>") ;
+            out.println( "<a href='Page4Servlet'>Click here for page4</a><br>") ;
+            out.println( "<a href='Page5Servlet'>Click here for page5</a><br>") ;
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
